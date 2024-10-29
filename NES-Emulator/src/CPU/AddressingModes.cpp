4acb3c3a1cf617eca9bb97ef434752df3a445f23
Natan822 <natanmiguel821@gmail.com>
2024-10-29 14:38:15 -0300

    fix: addressing mode functions now use readMemory function

--- NES-Emulator/src/CPU/AddressingModes.cpp
+++ NES-Emulator/src/CPU/AddressingModes.cpp
@@ -2,7 +2,7 @@
 #include "CPU.h"
 
 uint8_t CPU::immediate() {
-	uint8_t value = memory[pc + 1];
+	uint8_t value = readMemory(pc + 1);
 	return value;
 }
 
@@ -14,8 +14,8 @@ uint8_t* CPU::immediatePtr() {
 uint8_t CPU::zeroPage() {
 	cycles++;
 
-	uint8_t address = memory[pc + 1];
-	uint8_t value = memory[address];
+	uint8_t address = readMemory(pc + 1);
+	uint8_t value = readMemory(address);
 
 	return value;
 }
@@ -24,16 +24,14 @@ uint16_t CPU::zeroPagePtr() {
 	cycles++;
 
 	uint16_t address = memory[pc + 1];
-	//uint8_t* value = &(memory[address]);
-
 	return address;
 }
 
 uint8_t CPU::zeroPageX() {
 	cycles += 2;
 
-	uint8_t address = memory[pc + 1];
-	uint8_t value = memory[(address + xReg) & 0xFF];
+	uint8_t address = readMemory(pc + 1);
+	uint8_t value = readMemory((address + xReg) & 0xFF);
 
 	return value;
 }
@@ -42,16 +40,14 @@ uint16_t CPU::zeroPageXPtr() {
 	cycles += 2;
 
 	uint8_t address = memory[pc + 1];
-	//uint8_t* value = &(memory[(address + xReg) & 0xFF]);
-
 	return (address + xReg) & 0xFF;
 }
 
 uint8_t CPU::zeroPageY() {
 	cycles += 2;
 
-	uint8_t address = memory[pc + 1];
-	uint8_t value = memory[(address + yReg) & 0xFF];
+	uint8_t address = readMemory(pc + 1);
+	uint8_t value = readMemory((address + yReg) & 0xFF);
 
 	return value;
 }
@@ -68,11 +64,11 @@ uint16_t CPU::zeroPageYPtr() {
 uint8_t CPU::absolute() {
 	cycles += 2;
 
-	uint8_t lowByte = memory[pc + 1];
-	uint8_t highByte = memory[pc + 2];
+	uint8_t lowByte = readMemory(pc + 1);
+	uint8_t highByte = readMemory(pc + 2);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	uint8_t value = memory[address];
+	uint8_t value = readMemory(address);
 
 	return value;
 }
@@ -80,11 +76,10 @@ uint8_t CPU::absolute() {
 uint16_t CPU::absolutePtr() {
 	cycles += 2;
 
-	uint8_t lowByte = memory[pc + 1];
-	uint8_t highByte = memory[pc + 2];
+	uint8_t lowByte = readMemory(pc + 1);
+	uint8_t highByte = readMemory(pc + 2);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	//uint8_t* value = &(memory[address]);
 
 	return address;
 }
@@ -92,11 +87,11 @@ uint16_t CPU::absolutePtr() {
 uint8_t CPU::absoluteX() {
 	cycles += 2;
 
-	uint8_t lowByte = memory[pc + 1];
-	uint8_t highByte = memory[pc + 2];
+	uint8_t lowByte = readMemory(pc + 1);
+	uint8_t highByte = readMemory(pc + 2);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	uint8_t value = memory[(address + xReg) & 0xFFFF];
+	uint8_t value = readMemory((address + xReg) & 0xFFFF);
 
 	if ((lowByte + xReg) > 0xFF)
 	{
@@ -109,11 +104,10 @@ uint8_t CPU::absoluteX() {
 uint16_t CPU::absoluteXPtr() {
 	cycles += 2;
 
-	uint8_t lowByte = memory[pc + 1];
-	uint8_t highByte = memory[pc + 2];
+	uint8_t lowByte = readMemory(pc + 1);
+	uint8_t highByte = readMemory(pc + 2);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	//uint8_t* value = &(memory[(address + xReg) & 0xFFFF]);
 
 	if ((lowByte + xReg) > 0xFF)
 	{
@@ -126,11 +120,11 @@ uint16_t CPU::absoluteXPtr() {
 uint8_t CPU::absoluteY() {
 	cycles += 2;
 
-	uint8_t lowByte = memory[pc + 1];
-	uint8_t highByte = memory[pc + 2];
+	uint8_t lowByte = readMemory(pc + 1);
+	uint8_t highByte = readMemory(pc + 2);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	uint8_t value = memory[(address + yReg) & 0xFFFF];
+	uint8_t value = readMemory((address + yReg) & 0xFFFF);
 
 	if ((lowByte + yReg) > 0xFF)
 	{
@@ -143,8 +137,8 @@ uint8_t CPU::absoluteY() {
 uint16_t CPU::absoluteYPtr() {
 	cycles += 2;
 
-	uint8_t lowByte = memory[pc + 1];
-	uint8_t highByte = memory[pc + 2];
+	uint8_t lowByte = readMemory(pc + 1);
+	uint8_t highByte = readMemory(pc + 2);
 
 	uint16_t address = (highByte << 8) | lowByte;
 	//uint8_t* value = &(memory[(address + yReg) & 0xFFFF]);
@@ -160,13 +154,13 @@ uint16_t CPU::absoluteYPtr() {
 uint8_t CPU::indirectX() {
 	cycles += 4;
 
-	uint8_t addressPtr = (memory[pc + 1] + xReg) & 0xFF;
+	uint8_t addressPtr = (readMemory(pc + 1) + xReg) & 0xFF;
 
-	uint8_t lowByte = memory[addressPtr];
-	uint8_t highByte = memory[(addressPtr + 1) & 0xFF];
+	uint8_t lowByte = readMemory(addressPtr);
+	uint8_t highByte = readMemory((addressPtr + 1) & 0xFF);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	uint8_t value = memory[address];
+	uint8_t value = readMemory(address);
 
 	return value;
 }
@@ -174,13 +168,12 @@ uint8_t CPU::indirectX() {
 uint16_t CPU::indirectXPtr() {
 	cycles += 4;
 
-	uint8_t addressPtr = (memory[pc + 1] + xReg) & 0xFF;
+	uint8_t addressPtr = (readMemory(pc + 1) + xReg) & 0xFF;
 
-	uint8_t lowByte = memory[addressPtr];
-	uint8_t highByte = memory[(addressPtr + 1) & 0xFF];
+	uint8_t lowByte = readMemory(addressPtr);
+	uint8_t highByte = readMemory((addressPtr + 1) & 0xFF);
 
 	uint16_t address = (highByte << 8) | lowByte;
-	//uint8_t* value = &(memory[address]);
 
 	return address;
 }
@@ -188,13 +181,13 @@ uint16_t CPU::indirectXPtr() {
 uint8_t CPU::indirectY() {
 	cycles += 3;
 
-	uint8_t addressPtr = memory[pc + 1];
+	uint8_t addressPtr = readMemory(pc + 1);
 
-	uint8_t lowByte = memory[addressPtr];
-	uint8_t highByte = memory[(addressPtr + 1) & 0xFF];
+	uint8_t lowByte = readMemory(addressPtr);
+	uint8_t highByte = readMemory((addressPtr + 1) & 0xFF);
 
 	uint16_t address = ((highByte << 8) | lowByte);
-	uint8_t value = memory[(address + yReg) & 0xFFFF];
+	uint8_t value = readMemory((address + yReg) & 0xFFFF);
 
 	if ((lowByte + yReg) > 0xFF)
 	{
@@ -207,10 +200,10 @@ uint8_t CPU::indirectY() {
 uint16_t CPU::indirectYPtr() {
 	cycles += 3;
 
-	uint8_t addressPtr = memory[pc + 1];
+	uint8_t addressPtr = readMemory(pc + 1);
 
-	uint8_t lowByte = memory[addressPtr];
-	uint8_t highByte = memory[(addressPtr + 1) & 0xFF];
+	uint8_t lowByte = readMemory(addressPtr);
+	uint8_t highByte = readMemory((addressPtr + 1) & 0xFF);
 
 	uint16_t address = ((highByte << 8) | lowByte);
 	//uint8_t* value = &(memory[(address + yReg) & 0xFFFF]);
